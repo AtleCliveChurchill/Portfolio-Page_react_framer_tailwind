11ee71a1b136a85f4413e76011b3233e8bd0c023
AtleCliveChurchill <atleclivechurchill@gmail.com>
2024-09-17 18:29:33 +0900

    reverting final commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,19 +7,12 @@ import { FaInstagram } from "react-icons/fa"
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-      <img className="mx-2 w-10" src={logo} alt="logo" />
+      <img className="mx-2 w-10" src={logo} alt="logo"/>
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <a href="https://www.linkedin.com/in/atle-clive-churchill-7726481ba/" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin />
-      </a>
-      <a href="https://github.com/AtleCliveChurchill" target="_blank" rel="noopener noreferrer">
-        <FaGithub />
-      </a>
-      <a href="https://www.instagram.com/atle.clive/?locale=zh-hans" target="_blank" rel="noopener noreferrer">
-        <FaInstagram />
-      </a>
-
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaInstagram/>
     </div>
   </nav>
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,79 +1,76 @@
-import project1 from "../assets/projects/project-1.png";
-import project2 from "../assets/projects/project-2.png";
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.png";
-import project5 from "../assets/projects/project-5.png";
+import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `A passionate and driven software engineer specializing in Full Stack Development. With a strong foundation in both front-end and back-end technologies, I excel at building dynamic, responsive web applications that deliver seamless user experiences. My expertise includes working with modern frameworks and languages, optimizing performance, and ensuring code quality through best practices. I am dedicated to continuous learning and thrive in collaborative environments where I can contribute to innovative projects that push the boundaries of technology.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Node.js, MySQL, PostgreSQL, SpringBoot and 3JS via CesiumJS. My journey in software development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "Feb 2023 - 2023",
-    role: "Software Developer",
-    company: "Fion Systems",
-    description: `Full Stack WebGIS Development by ReactJS integrated with CesiumJS for
-3D Map Rendering and Springboot with PostgreSQL, Development of backend APIs with ExpressJS, Node, Python,  Redesign of Company Website with wordpress, PHPMyAdmin and MySQL`,
-    technologies: ["HTML", "CSS", "JavaScript", "React", "ExpressJS", "CesiumJS", "PHP", "PostgreSQL", "NodeJS", "Python", "Springboot"],
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
-    year: "July 2022 - December 2023",
-    role: "Team Leader",
-    company: "International Writing Center",
-    description: `Mentor students with English communication, writing and resume skills, Spearheaded the Redesign and Front-End modification of Webpage, Managed the scheduling and Back-End of Datasheets, Digital Marketing: Promoted Social Media Reach, Content Creation: Designed creative content and Videos to aid in
-functionality and training`,
-    technologies: ["PHP", "JavaScript", "Office Suite", "Wix", "Adobe", "Wordpress", "Canva"],
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   {
-    year: "May 2021 - June 2021",
-    role: "Web Development Intern",
-    company: "The Sparks Foundation - India",
-    description: `Complete FrontEnd Design and Animation of Oxygen Donation Page, Demo RazorPay Integration for payment API`,
-    technologies: ["HTML", "CSS", "JavaScript", "Custom RazorPay API"],
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "React & Framer-Motion WebPage",
+    title: "E-Commerce Website",
     image: project1,
     description:
-      "A Responsive WebPage to Showcase personal skills and projects along with personal details and contact information",
-    technologies: ["HTML", "CSS", "React", "TailWind", "JavaScript", "Framer-Motion"],
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "React & Material UI Responsive Design Hotel WebDesign",
+    title: "Task Management App",
     image: project2,
     description:
-      "A fully responsive webpage using React and Material UI to adapt to different viewports and easy custom design",
-    technologies: ["HTML", "CSS", "React", "MaterialUI", "JavaScript"],
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
-    title: "AI Disease Identification from Potato Leaf Images",
+    title: "Portfolio Website",
     image: project3,
     description:
-      "An AI CNN Model Trained to Identify Blight Diseases among Potato Plants by Uploading the Images through a React Front-End and Fast-API",
-    technologies: ["HTML", "CSS", "JavaScript", "React", "Python", "FastAPI"],
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "PHP Cafetaria Website",
+    title: "Blogging Platform",
     image: project4,
     description:
-      "A fully functioning website with HTML, CSS, PHP and Database to store and retreive information",
-    technologies: ["HTML", "CSS", "PHP", "MySQL"],
-  },
-  {
-    title: "Front-End Web Page",
-    image: project5,
-    description:
-      "A basic front-end with vanilla HTML & CSS to grasp the fundamentals, along with Razor-Pay API Intergation ",
-    technologies: ["HTML", "CSS", "JavaScript"],
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
   },
 ];
 
 export const CONTACT = {
-  address: "Yatap-ro, Bundang-gu, Seongnam-si, Gyeonggi-do, South Korea",
-  phoneNo: "+82 010 6617 5535",
-  email: "atleclivechurchill@gmail.com",
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
 };
